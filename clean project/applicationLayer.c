0ac56eae4d9d99e8309434eeb3efc10721c36d7b
charchalis <charchalis97@gmail.com>
2023-10-27 15:43:13 +0100

    make file

--- clean project/applicationLayer.c
+++ clean project/applicationLayer.c
@@ -15,47 +15,11 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename){
 
-    LinkLayer linkLayer;
-    strcpy(linkLayer.serialPort,serialPort);
-    linkLayer.role = strcmp(role, "tx") ? receiver : transmitter;
-    linkLayer.baudRate = baudRate;
-    linkLayer.nRetransmissions = nTries;
-    linkLayer.timeout = timeout;
+    printf("popoaa\n");
+    
 
-    int fd = llopen(linkLayer);
-    if (fd < 0) {
-        printf("Connection failed\n");
-        return;
-    } 
 
-    switch (linkLayer.role) {
+    //----------------codigo da gaja---------------//
 
-        case transmitter: {
-            
-            FILE* file = fopen(linkLayer.filename);
-            
-            if(llwrite(fd, controlPacket, cpSize) == -1) { 
-                printf("Exit: error in end packet\n");
-                exit(-1);
-            }
-            llclose(fd);
-            break;
-        }
-
-        case receiver: {
-
-            FILE* newFile = fopen((char *) name, "wb+");
-
-                while (llread(fd, packet) < 0){
-                    
-                }
-            }
-
-            fclose(newFile);
-            break;
-
-        default:
-            exit(-1);
-            break;
-    }
+    
 }
